e660178936ed5f54b5572a46fa10d63bc636d172
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2015-05-19 17:01:30 +0200

    Create a info-dynamic-condor component to display Glue1 / Glue2 output
    Fix some issue w/ old perl component

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='Condor-Tools',
       description='Set of python script to interact with HTCondor',
       author='Guillaume Philippon',
       author_email='guillaume.philippon@lal.in2p3.fr',
-      scripts=["csub"],
+      scripts=["csub", "info-dynamic-condor"],
       dependency_links=['http://research.cs.wisc.edu/htcondor/downloads/'
                         '?state=select_from_mirror_page&version=8.2.7&'
                         'mirror=UW%20Madison&optional_organization_url=http://'],
